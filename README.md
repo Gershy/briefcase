fa56a5c9c2f76ce132d75cfdbb62ce0d22683f18
Daniel Max <danielmaxsiegel@gmail.com>
2018-09-15 21:24:48 -0700

    added ability to start server on any port with env variable

---
+++ README.md
@@ -0,0 +1,16 @@
+# WHAT'S IN THE BRIEFCASE??
+
+The future of snacks is now.
+
+## Instructions
+To run the server:
+```
+$ node briefcase.js
+```
+
+This will start the app on port 80. If you want to run it on another port, for example 3456, run:
+```
+$ PORT=3456 node briefcase.js
+```
+
+to set the env variables.
--- briefcase.js
+++ briefcase.js
@@ -3,95 +3,95 @@ let fs = require('fs-extra');
 let path = require('path');
 
 (async () => {
-  
+
   let clientHtmlPath = path.join(__dirname, 'client.html');
   let serverActive = null;
   let clientHtml = null;
-  
+
   let server = http.createServer(async (request, response) => {
-    
+
     console.log(request.method, request.url);
-    
+
     let method = request.method.toLowerCase();
     let url = request.url;
-    
+
     try {
-      
+
       if (method === 'get' && url === '/') {
-        
+
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.end(clientHtml);
-        
+
       } else if (method === 'get' && url === '/favicon') {
-        
+
         response.writeHead(404, { 'Content-Type': 'text/plain' });
         response.end('No favicon yet :(');
-        
+
       } else if (method === 'get' && url.startsWith('/assets')) {
-        
+
         let pcs = url.split('/');
         let lastPc = pcs[pcs.length - 1];
-        
+
         console.log('LAST:', lastPc);
-        
+
         if (!~lastPc.indexOf('.')) throw new Error(`Bad url: ${url}`);
-        
+
         lastPc = lastPc.split('.');
         let ext = lastPc[lastPc.length - 1];
-        
+
         let contentType = ({
           png: 'image/png',
           jpg: 'image/jpg',
           jpeg: 'image/jpg'
         })[ext];
-        
+
         if (!contentType) throw new Error(`Bad asset extension: ${ext}`);
-        
+
         response.writeHead(200, { 'Content-Type': contentType });
-        
+
         let filepath = path.join(__dirname, ...pcs);
         fs.createReadStream(filepath).pipe(response);
-        
+
       } else {
-        
+
         throw new Error('bad request');
-        
+
       }
-      
+
       console.log(`served: ${method} ${url}`);
-      
+
     } catch(err) {
-      
+
       response.writeHead(404, { 'Content-Type': 'text/plain' });
       response.end(`dunno: ${method} ${url}`);
       console.log(`error: ${method} ${url} (${err.message})`);
       return;
-      
+
     }
-    
+
   });
-  
-  let port = 80;
+
+  let port = process.env.PORT || 80;
   let host = 'localhost';
-  
+
   [ serverActive, clientHtml ] = await Promise.all([
     new Promise(r => server.listen(port, host, r)),
     (async () => {
-      
+
       let [ clientHtml, clientCss, clientJs ] = await Promise.all([
         fs.readFile(path.join(__dirname, 'client', 'client.html'), 'utf8'),
         fs.readFile(path.join(__dirname, 'client', 'client.css'), 'utf8'),
         fs.readFile(path.join(__dirname, 'client', 'client.js'), 'utf8')
       ]);
-      
+
       let replacements = {
         css: clientCss,
         js: clientJs
       };
       for (const [placeholder, content] of Object.entries(replacements)) {
-        
+
         let pcs = clientHtml.split(`{{${placeholder}}}`);
-        
+
         let newHtml = [];
         for (let i = 0; i < pcs.length - 1; i++) {
           let pc = pcs[i];
@@ -100,22 +100,20 @@ let path = require('path');
           let spaces = ' '.repeat(pc.length - ind);
           let indented = content.trim().split('\n').map((line, ind) => !ind ? line : spaces + line).join('\n');
           newHtml.push(pc, indented);
-          
+
         }
         newHtml.push(pcs[pcs.length - 1]);
-        
+
         clientHtml = newHtml.join('');
-        
+
       }
-      
+
       return clientHtml;
-      
+
     })()
   ]);
-  
+
   console.log('Server ready');
-  
-  
+
+
 })();
-
-
